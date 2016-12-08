d5287f80c27a64ae8567f945c843ee7ec1393040
Matthew McLean <matthewmclean21@gmail.com>
2016-12-07 17:23:42 -0800

    Added create action to save to db

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,3 +1,9 @@
-class Contact < ActiveRecord::Base
+# Model files like this are the blueprint for objects on the website.
+# For instance, this is where we'll restrict user from submitting an incorrect
+# value for email (must have @ and .something)
 
+class Contact < ActiveRecord::Base
+# Don't have to include basic instance variables, as Ruby already knows to set
+# up for them based on the schema.rb file, where we already stated what values
+# are being collected.
 end
