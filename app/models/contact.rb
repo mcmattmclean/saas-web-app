d099a3c659e3cd61e652903c1ef8289edef8d8cf
Matthew McLean <matthewmclean21@gmail.com>
2016-12-12 13:47:27 -0800

    Added validation for form to model

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -6,4 +6,7 @@ class Contact < ActiveRecord::Base
 # Don't have to include basic instance variables, as Ruby already knows to set
 # up for them based on the schema.rb file, where we already stated what values
 # are being collected.
+  validates :name, presence: true
+  validates :email, presence: true
+  validates :comments, presence: true
 end
